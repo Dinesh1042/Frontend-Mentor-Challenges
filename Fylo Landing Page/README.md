73d5c06cb53f71bcd7d3348b1e5608902cd6dd8b
Dinesh <70156399+Dinesh1042@users.noreply.github.com>
2021-05-06 15:57:05 +0530

    Fylo Landing Page

---
+++ Fylo Landing Page/README.md
@@ -0,0 +1,25 @@
+# Fylo Landing Page - [FrontendMentor](https://www.frontendmentor.io/challenges/fylo-landing-page-with-two-column-layout-5ca5ef041e82137ec91a50f5)
+
+![FyloLandingPage](./screenshot/Fylo.png)
+
+### Links
+
+- [Github](https://your-solution-url.com)
+- [Fylo Live Site]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vannila Javascript project
+- **NO Frameworks Used**
+
+## Author
+
+- Website - [Fylo Landing Page](https://www.your-site.com)
+- Frontend Mentor - [Dinesh1042](https://www.frontendmentor.io/profile/Dinesh1042)
---
+++ Fylo Landing Page/app/Js/script.js
@@ -0,0 +1,28 @@
+const formsEl = document.querySelectorAll("#form");
+
+formsEl.forEach((form) => form.addEventListener("submit", validateForm));
+
+function validateForm(e) {
+  e.preventDefault();
+  const inputEl = this.querySelector("input");
+  const errorMsg = this.querySelector(".errorMsg");
+  const inputValue = inputEl.value.trim();
+
+  if (inputValue) {
+    const isValidEmail = emailValidation(inputValue);
+    if (isValidEmail) {
+      this.classList.remove("error");
+    } else {
+      this.classList.add("error");
+      errorMsg.innerHTML = `Please check your email`;
+    }
+  } else {
+    this.classList.add("error");
+    errorMsg.innerHTML = `User email cannot be empty`;
+  }
+}
+
+function emailValidation(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
